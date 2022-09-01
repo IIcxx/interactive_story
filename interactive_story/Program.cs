af4eea9144ab18c3bc855d28c6a0ebde016e23b0
Viktor Karlsson <viktor.karlsson2@elev.ga.ntig.se>
2022-09-01 16:26:53 +0200

    initial commit

---
+++ interactive_story/Program.cs
@@ -0,0 +1,27 @@
+﻿string p1 = "null";
+string p21 = "null";
+string p22 = "null";
+string p23 = "null";
+string p311 = "null";
+string p312 = "null";
+Console.WriteLine("You are presented with two roads, which one do you take, left or right?");
+
+
+p1 = Console.ReadLine();
+
+if (p1 == "right")
+{
+    Console.WriteLine("ok man");
+}
+
+else if (p1 == "left" )
+{
+    Console.WriteLine("");
+}
+
+else
+{ 
+    Console.WriteLine("ok i see you tryna be funny with it, don't");
+}
+
+Console.ReadLine();
